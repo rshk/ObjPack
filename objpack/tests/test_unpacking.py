4e460c7922aec1044314dd4652f0bd2db424cd4a
Samuele Santi <redshadow@hackzine.org>
2013-11-02 02:13:22 +0100

    Better separation between tests

--- objpack/tests/test_unpacking.py
+++ objpack/tests/test_unpacking.py
@@ -7,6 +7,8 @@ def test_unpack_simple():
     assert objpack.loads('3.14') == 3.14
     assert objpack.loads('1024') == 1024
 
+
+def test_unpack_strings():
     # Check simple strings
     assert objpack.loads('"this is a string"') == "this is a string"
     assert objpack.loads("'this is a string'") == "this is a string"
@@ -28,17 +30,23 @@ def test_unpack_simple():
     #assert objpack.loads('"This is \\n a newline"') == "This is \n a newline"
     #assert objpack.loads('"This is \\t a tab"') == "This is \t a tab"
 
+
+def test_unpack_bool_null():
     # Boolean and None types..
     assert objpack.loads('true') is True
     assert objpack.loads('false') is False
     assert objpack.loads('null') is None
 
+
+def test_unpack_lists():
     # Fun with lists
     assert objpack.loads('[]') == []
     assert objpack.loads('[1, 2, 3]') == [1, 2, 3]
     assert objpack.loads('["a", "b", "c"]') == ["a", "b", "c"]
     assert objpack.loads('[1, "abc", true, []]') == [1, 'abc', True, []]
 
+
+def test_unpack_dicts():
     # Fun with dicts
     assert objpack.loads('{}') == {}
     assert objpack.loads("{'this': 'is', 'an': 'example'}") \
@@ -46,6 +54,8 @@ def test_unpack_simple():
     assert objpack.loads("{'one': 1, 'two': 3, 'three': [1, 2, 3]}") \
         == {'one': 1, 'two': 3, 'three': [1, 2, 3]}
 
+
+def test_unpack_objects():
     # Fun with objects
     assert objpack.loads("Hello()") == Node('Hello')
     assert objpack.loads("Hello('world')") == Node('Hello', 'world')
@@ -58,6 +68,8 @@ def test_unpack_simple():
     assert objpack.loads("Hello(\n\texample=1\n)") \
         == Node('Hello', example=1)
 
+
+def test_unpack_comments():
     # Comments should be ignored
     assert objpack.loads("""
     # This is a comment and should be ignored.
